b8ecf27f0ad4aa3db8e083560db1a8dc96fe6b6b
HugoWiltEFREI <150117113+HugoWiltEFREI@users.noreply.github.com>
2024-05-19 11:53:53 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "linkedList.h"
 
 
-Data getInput(int type, char *input);
+/*Data getInput(int type, char *input);
 
 int main() {
     int partSelection, functionSelection, type, numberOfValues, index, category, size;
@@ -66,7 +66,7 @@ int main() {
         float_test.type = FLOAT_TYPE;
         float_test.value.float_type = 3.14;
         string_test.type = STRING_TYPE;
-        strcpy((string_test.value.string_type), "Test");
+        strcpy(&(string_test.value.string_type), "Test");
         insert_value(mycol, int_test);
         insert_value(mycol, char_test1);
         insert_value(mycol, int_test2);
@@ -103,7 +103,7 @@ int main() {
                     printf("%lf\n",val.value.double_type);
                     break;
                 case STRING_TYPE :
-                    printf("%s\n",(val.value.string_type));
+                    printf("%s\n",&(val.value.string_type));
                     break;
                 default :
                     printf("ERROR : unknown type.\n");
@@ -164,7 +164,7 @@ int main() {
                 printf("Enter new title : ");
                 scanf("%s", newTitle);
                 printf("%s %s\n", oldTitle, newTitle);
-                renameColumn(&myliste,oldTitle, newTitle);
+                //renameColumn(&myliste,oldTitle, newTitle);
                 displayList(myliste);
             }
             if (functionSelection == 7){
@@ -240,23 +240,22 @@ int main() {
                 hardFillLinked(&myliste);
                 printf("Enter a value to compare with :");
                 Data val = getInput(type, input);
-                printf("There are %d cells equal to val", numberOfCellsUnderVal(myliste, val));
+                printf("There are %d cells under val", numberOfCellsUnderVal(myliste, val));
             }
             if (functionSelection == 5){
                 hardFillLinked(&myliste);
                 printf("Enter a value to compare with :");
                 Data val = getInput(type, input);
-                printf("There are %d cells equal to val", numberOfCellsAboveVal(myliste, val));
+                printf("There are %d cells above val", numberOfCellsAboveVal(myliste, val));
             }
 
         }
     } if (partSelection == 3){
-        COLUMN *mycol = createcolumnPart2(INT_TYPE, "My Column");
-        Data val;
-        val.type = INT_TYPE;
-        val.value.int_type = 31;
-        mycol->data[0] = val;
-        printcolPart2(mycol);
+        COLUMN *mycol = createcolumnPart2(CHAR_TYPE, "My Column");
+        char a = 'A', c = 'C';
+        insertvaluePart2(mycol, &a);
+        insertvaluePart2(mycol, NULL);
+        insertvaluePart2(mycol, &c);
 
     }
     return 0;
@@ -286,4 +285,26 @@ Data getInput(int type, char *input) {
             val.value.char_type = ' ';
     }
     return val;
+}*/
+int main(){
+    char str[5];
+    COLUMN *mycol = createcolumnPart2(INT_TYPE, "My column");
+    int a = 52, b = 44, c = 15, d = 18, e = 15, f = 15, g =12, h = 3;
+    insertvaluePart2(mycol, &a);
+    insertvaluePart2(mycol, &b);
+    insertvaluePart2(mycol, &c);
+    insertvaluePart2(mycol, &d);
+    insertvaluePart2(mycol, &e);
+    insertvaluePart2(mycol, &f);
+    insertvaluePart2(mycol, &g);
+    insertvaluePart2(mycol, &h);
+
+    convert_value(mycol, 2, str, 5);
+    printf("%s \n", str);
+    printf("%d ", occurrencePart2(mycol, &c));
+    printf("%d ", numberOfValuesAboveSearchValuePart2(mycol, &c));
+    printf("%d\n", numberOfValuesUnderSearchValuePart2(mycol, &c));
+    printValueAtIndex(mycol, 3);
+    return 0;
 }
+
